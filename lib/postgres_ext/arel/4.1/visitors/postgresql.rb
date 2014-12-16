5a40bf6179d17cd37b1368c967914b40b706aa6b
Edward Paget <edward@cassetteta.pe>
2014-12-16 13:41:49 -0600

    Fix WhereChain operators to work on joins
    Regular ActiveRecord where queries can work on joined relations, ie
    `Relation.joins(:join).where(join: { has_value: 1 })`, which also
    works with the the WhereChain `#not` method. This adapts the `#overlap`
    and `#contains_*` methods added in this gem to also work on join queries.

--- lib/postgres_ext/arel/4.1/visitors/postgresql.rb
+++ lib/postgres_ext/arel/4.1/visitors/postgresql.rb
@@ -13,6 +13,16 @@ module Arel
           o.empty? ? 'NULL' : o.map { |x| visit x }.join(', ')
         end
       end
+
+      def visit_Arel_Nodes_Contains o, a = nil
+        left_column = o.left.relation.engine.columns.find { |col| col.name == o.left.name.to_s }
+        
+        if left_column && (left_column.type == :hstore || (left_column.respond_to?(:array) && left_column.array))
+          "#{visit o.left, a} @> #{visit o.right, o.left}"
+        else
+          "#{visit o.left, a} >> #{visit o.right, o.left}"
+        end
+      end
       
       def visit_Arel_Nodes_ContainedWithin o, a = nil
         "#{visit o.left, a} << #{visit o.right, o.left}"
@@ -22,14 +32,16 @@ module Arel
         "#{visit o.left, a} <<= #{visit o.right, o.left}"
       end
 
-      def visit_Arel_Nodes_Contains o, a = nil
-        left_column = o.left.relation.engine.columns.find { |col| col.name == o.left.name.to_s }
+      def visit_Arel_Nodes_ContainsArray o, a = nil
+        "#{visit o.left, a} @> #{visit o.right, o.left}"
+      end
 
-        if left_column && (left_column.type == :hstore || (left_column.respond_to?(:array) && left_column.array))
-          "#{visit o.left, a} @> #{visit o.right, o.left}"
-        else
-          "#{visit o.left, a} >> #{visit o.right, o.left}"
-        end
+      def visit_Arel_Nodes_ContainsHStore o, a = nil
+        "#{visit o.left, a} @> #{visit o.right, o.left}"
+      end
+
+      def visit_Arel_Nodes_ContainsINet o, a = nil
+        "#{visit o.left, a} >> #{visit o.right, o.left}"
       end
 
       def visit_Arel_Nodes_ContainsEquals o, a = nil
